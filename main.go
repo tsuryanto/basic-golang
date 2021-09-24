5c5ff0afafd8a77efd793ad01dd679bb19cd79c9
tsuryanto <tsuryanto16@gmail.com>
2021-09-24 16:42:46 +0700

    Belajar jenis-jenis function di Golang

--- main.go
+++ main.go
@@ -13,8 +13,21 @@ func main() {
 	// LatihanSliceMap()
 	// Quiz2()
 
-	printMyResult()
+	printMyResult("Saya sedang belajar Golang")
+
+	// luas, keliling := calculate(10, 2)
+	luas, _ := calculate(10, 2)
+	fmt.Println("Luas : ", luas)
+	// fmt.Println(keliling)
+
+	// luas, keliling := calculate(10, 2)
+	luas, keliling := prCalculate(20, 4)
+	fmt.Println("PR : ", luas)
+	fmt.Println("PR : ", keliling)
+
 }
+
+/* FUNCTION */
 
 /* =================
 Pada Function terdapat :
@@ -23,6 +36,23 @@ Pada Function terdapat :
 3. hasil / return value
 ================= */
 
-func printMyResult() {
-	fmt.Println("Saya sedang belajar Go")
+func printMyResult(sentence string) {
+	fmt.Println(sentence)
+}
+
+// Multiple Return Value
+func calculate(panjang int, lebar int) (int, int) {
+	luas := panjang * lebar
+	keliling := 2 * (panjang + lebar)
+
+	return luas, keliling
+}
+
+// Predefined Return Value
+// mendefinisikan fariabel sejajar dengan function
+func prCalculate(panjang int, lebar int) (luas int, keliling int) {
+	luas = panjang * lebar
+	keliling = 2 * (panjang + lebar)
+
+	return luas, keliling
 }
